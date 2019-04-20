5a804f79f4227d75b49bcdc56962a28a6a90311c
Elena <elenasi@kth.se>
2019-04-20 09:55:00 +0200

    clean up

--- tab.ps1
+++ tab.ps1
@@ -1,5 +1,3 @@
 	$wshell=New-Object -ComObject wscript.shell;
 	$wshell.AppActivate('Google Chrome'); # Activate on Opera browser
-	#Sleep 1; # Interval (in seconds) between switch 
-	#$wshell.SendKeys('^{TAB}'); # Ctrl + Page Up keyboard shortcut to switch tab
-	$wshell.SendKeys('^{PGDN}'); # F5 to refresh active page
+	$wshell.SendKeys('^{PGDN}'); # Ctrl+Page Down to switch a tab
